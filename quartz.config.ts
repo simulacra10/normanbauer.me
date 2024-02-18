d06cbf273d12f4ec94edaffcff73daaadb86c943
Norman Bauer <normanabuer@gmail.com>
2024-02-18 18:46:08 -0500

    Quartz sync: Feb 18, 2024, 6:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "{normanbauer.me}",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
