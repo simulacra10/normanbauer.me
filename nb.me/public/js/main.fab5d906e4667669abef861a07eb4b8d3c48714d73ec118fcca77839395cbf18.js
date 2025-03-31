114965b3515f6029c7a6c490523bad6bc4a468e1
Norman Bauer <normanbauer@gmail.com>
2025-03-30 20:31:31 -0400

    Deleted temp directory and renamed archive.

--- nb.me/public/js/main.fab5d906e4667669abef861a07eb4b8d3c48714d73ec118fcca77839395cbf18.js
+++
@@ -1,1 +0,0 @@
-(()=>{document.addEventListener("DOMContentLoaded",function(){var e=document.getElementById("sidebar"),t=document.querySelector("header button");e&&t&&t.addEventListener("click",function(){e.classList.toggle("show")})});})();
